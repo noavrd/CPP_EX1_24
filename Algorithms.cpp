5dd6c2ea27c00c480bf1867b8eb57f579a6e3f77
noavrd <noavrd@gmail.com>
2024-09-01 18:33:05 +0300

    add tests

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,6 +1,7 @@
 // noavrd@gmail.com
 #include "Algorithms.hpp"
 #include <list>
+
 #define true 1
 #define false 0
 #define INT_MAX 999
@@ -14,7 +15,8 @@ using namespace ariel;
  *  This function checks if all parts of a graph are connected. It starts from the first point and explores all connected points. 
  *  If it can reach all of the points, the graph is connected. 
  *  If not, the graph is disconnected. 
- *
+ * 
+ *  @return 1 if the graph is conneced, if not it will return 0.
  */
 int Algorithms::isConnected(const Graph &g) {
     // get the number of vertices in a graph
@@ -57,8 +59,9 @@ int Algorithms::isConnected(const Graph &g) {
  *  Check what is the shortest path between a start and an end node in a graph using BFS.
  *
  *  It using a list that helps keep track which nodes needs to be visited next.
- *  It checks each node's neighbors one by one. If there is a path it will return the path, if not it will return -1.
- *
+ *  It checks each node's neighbors one by one.
+ *  
+ *  @return If there is a path it will return the path, if not it will return -1.
  */
 string Algorithms::shortestPath(const Graph &g, size_t start, size_t end){
     size_t size = g.graphSize();
@@ -115,17 +118,29 @@ string Algorithms::shortestPath(const Graph &g, size_t start, size_t end){
     return pathString;
 }
 
-int Algorithms::isContainsCycle( const Graph &g ) { 
+/**
+ *  Checks if the graph contains a cycle.
+ * 
+ *  This function uses DFS to find cycles in a graph.
+ *  It uses a visited list and a parent list to keep track of nodes and their parents. 
+ *  If we find an edge that connects back to a node that's not its direct parent, it means there's a cycle in the graph.
+ * 
+ *  @return 1 if a cycle is found, otherwise 0.
+ */
+int Algorithms::isContainsCycle(const Graph &g) { 
     size_t size = g.graphSize();
     vector<vector<int>> adjMatrix = g.getMatrix();
 
-    vector<bool> visited(size, 0);
-    vector<size_t> parent(size, (size_t)-1);
+    // Keeps track of visited nodes
+    vector<bool> visited(size, 0); 
+
+    // Stores the parent of each node
+    vector<size_t> parent(size, (size_t)-1); 
 
     for (size_t i = 0; i < size; ++i) {
         if (!visited[i]) {
-
-            list<size_t> verticsToCheck;
+            // List to use in DFS
+            list<size_t> verticsToCheck; 
             verticsToCheck.push_back(i);
             
             while (!verticsToCheck.empty()) {
@@ -135,12 +150,15 @@ int Algorithms::isContainsCycle( const Graph &g ) {
                 if (!visited[node]) {
                     visited[node] = 1;
                     for (size_t j = 0; j < size; ++j) {
-                        if (adjMatrix[node][j] != 0) {
+                        // Check for nodes near the current one
+                        if (adjMatrix[node][j] != 0) { 
                             if (!visited[j]) {
                                 verticsToCheck.push_back(j);
                                 parent[j] = node;
+
+                            // A cycle has been found
                             } else if (parent[node] != j) {
-                                return true;
+                                return true; 
                             }
                         }
                     }
@@ -149,16 +167,26 @@ int Algorithms::isContainsCycle( const Graph &g ) {
         }
     }
 
-    return false;
+    return false; 
 }
 
-
-string Algorithms::isBipartite( const Graph &g ) {
+/**
+ *  Checks if the graph is bipartite.
+ * 
+ *  This function checks if the graph can be colored using two colors such that no two nodes next to each node
+ *  have the same color. It uses BFS to try and assign colors to each node. If any two nodes near eachothers have
+ *  the same color, the graph is not bipartite.
+ * 
+ *  @return A string that shows if the graph is bipartite and listing the two sets of nodes.
+ *  If it's not bipartite it will return 0.
+ */
+string Algorithms::isBipartite(const Graph &g) {
     size_t size = g.graphSize();
     vector<vector<int>> adjMatrix = g.getMatrix();
     
+    // Color assignment for nodes, and give the first node color 0
     vector<int> twoGroups(size, -1);
-    twoGroups[0] = 0;
+    twoGroups[0] = 0; 
 
     list<size_t> verticsToCheck;  
     verticsToCheck.push_back(0);
@@ -167,29 +195,34 @@ string Algorithms::isBipartite( const Graph &g ) {
         size_t node = verticsToCheck.front();
         verticsToCheck.pop_front();
 
-        for (size_t i = 0; i < size; ++i) {
-            if (adjMatrix[node][i] != 0) {
-                if (twoGroups[i] == -1) {
+         for (size_t i = 0; i < size; ++i) {
+            // Check nodes near each other
+            if (adjMatrix[node][i] != 0) { 
+            // If node is not colored yet
+                if (twoGroups[i] == -1) { 
+                    // give ulternative color
                     twoGroups[i] = 1 - twoGroups[node];
                     verticsToCheck.push_back(i);
                 } else if (twoGroups[i] == twoGroups[node]) {
-                        return "0"; 
+                    // if 2 nodes near each other has the same color, it's not bipartite
+                    return "0"; 
                 }
             }
         }
     }
 
+    // create the sets of nodes for each color group.
     vector<int> firstSide, secondSide;
 
     for (size_t i = 0; i < size; ++i){
         if (twoGroups[i] == 0) {
             firstSide.push_back(i);
-        }
-        else {
+        } else {
             secondSide.push_back(i);
         }
     }
 
+    // create the string for the two different color if it's bipartite
     string result = "The graph is bipartite: A={";
 
     for (size_t i = 0; i < firstSide.size(); ++i) {
@@ -212,17 +245,28 @@ string Algorithms::isBipartite( const Graph &g ) {
     return result;
 }
 
-string Algorithms::negativeCycle( const Graph &g ) {
-    if(!isContainsCycle(g)) {
-        return "There is no negative cycle";
+/**
+ *  Check if there is a negative weight cycle in the graph.
+ * 
+ *  This function uses the Bellman-Ford algorithm to detect negative weight cycles. It first checks
+ *  if there is any cycle in the graph using isContainsCycle.
+ *  Then, it calculates the shortest paths and checks for negative weight cycles by running an extra iteration.
+ * 
+ *  @return A string of the negative cycle if it's exist 0 if there is no negative cycle.
+ */
+string Algorithms::negativeCycle(const Graph &g) {
+    if (!isContainsCycle(g)) {
+        return "0";
     }
 
     size_t size = g.graphSize();
     vector<vector<int>> adjMatrix = g.getMatrix();
 
+    // initialize distance from the starting node
     vector<int> distance(size, -1); 
-    distance[0] = 0;
+    distance[0] = 0; 
 
+    // check distance for each node
     for (size_t i = 0; i < size - 1; ++i) {
         for (size_t w = 0; w < size; ++w) {
             for (size_t v = 0; v < size; ++v) {
@@ -235,15 +279,16 @@ string Algorithms::negativeCycle( const Graph &g ) {
         }
     }
 
+    // Check for negative weight cycles.
     for (size_t w = 0; w < size; ++w) {
         for (size_t v = 0; v < size; ++v) {
             if (adjMatrix[w][v] != 0) {
                 if (distance[w] != -1 && distance[w] + adjMatrix[w][v] < distance[v]) {
-                    return "found from "+ std::to_string(distance[w]);
+                    return "Negative cycle: " + std::to_string(distance[w]);
                 }
             }
         }
     }
 
-    return "There is no negative cycle";
+    return "0"; 
 }
--- Test.cpp
+++ Test.cpp
@@ -82,14 +82,6 @@ TEST_CASE("Test isBipartite")
     g.loadGraph(graph2);
     CHECK(ariel::Algorithms::isBipartite(g) == "0");
 
-    vector<vector<int>> graph3 = {
-        {0, 1, 2, 0, 0},
-        {1, 0, 3, 0, 0},
-        {2, 3, 0, 4, 0},
-        {0, 0, 4, 0, 5},
-        {0, 0, 0, 5, 0}};
-    g.loadGraph(graph3);
-    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2, 4}, B={1, 3}");
 }
 TEST_CASE("Test invalid graph")
 {
@@ -100,5 +92,325 @@ TEST_CASE("Test invalid graph")
         {2, 3, 0, 4},
         {0, 0, 4, 0},
         {0, 0, 0, 5}};
+    CHECK_THROWS(g.loadGraph(graph));
+}
+
+
+TEST_CASE("Test isConnected with simple graph") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isConnected(g) == true);
+}
+
+TEST_CASE("Test isConnected with disconnected graph") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 1},
+        {0, 0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isConnected(g) == false);
+}
+
+TEST_CASE("Test shortestPath with direct path") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "0->1->2");
+}
+
+TEST_CASE("Test shortestPath with no path") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "-1");
+}
+
+TEST_CASE("Test isContainsCycle with no cycle") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+}
+
+TEST_CASE("Test isContainsCycle with cycle") {
+    ariel::Graph g;
+
+    vector<vector<int>> graph = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == 1); 
+}
+
+TEST_CASE("Test isBipartite with bipartite graph") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2}, B={1}");
+}
+
+TEST_CASE("Test isBipartite with non-bipartite graph") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isBipartite(g) == "0");
+}
+
+TEST_CASE("Test addition of two graphs") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {0, 1, 2},
+        {1, 0, 3},
+        {2, 3, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 1},
+        {1, 0, 2},
+        {1, 2, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph result = g1 + g2;
+    vector<vector<int>> expected = {
+        {0, 2, 3},
+        {2, 0, 5},
+        {3, 5, 0}};
+    CHECK(result.getMatrix() == expected);
+}
+
+TEST_CASE("Test subtraction of two graphs") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {0, 2, 3},
+        {2, 0, 5},
+        {3, 5, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 1},
+        {1, 0, 2},
+        {1, 2, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph result = g1 - g2;
+    vector<vector<int>> expected = {
+        {0, 1, 2},
+        {1, 0, 3},
+        {2, 3, 0}};
+    CHECK(result.getMatrix() == expected);
+}
+
+TEST_CASE("Test multiplication of two graphs") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {0, 1},
+        {1, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 2},
+        {2, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph result = g1 * g2;
+    vector<vector<int>> expected = {
+        {0, 0},
+        {0, 0}}; // Ensure this is the correct expected result
+
+    auto resultMatrix = result.getMatrix();
+    auto expectedMatrix = expected;
+
+    for (size_t i = 0; i < resultMatrix.size(); ++i) {
+        for (size_t j = 0; j < resultMatrix[i].size(); ++j) {
+            CHECK(resultMatrix[i][j] == expectedMatrix[i][j]);
+        }
+    }
+}
+
+TEST_CASE("Test scalar multiplication") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {1, 2},
+        {3, 4}};
+    g.loadGraph(graph);
+
+    ariel::Graph result = g * 2;
+    vector<vector<int>> expected = {
+        {2, 4},
+        {6, 8}};
+    CHECK(result.getMatrix() == expected);
+}
+
+TEST_CASE("Test scalar division") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {2, 4},
+        {6, 8}};
+    g.loadGraph(graph);
+
+    ariel::Graph result = g /= 2;
+    vector<vector<int>> expected = {
+        {1, 2},
+        {3, 4}};
+    CHECK(result.getMatrix() == expected);
+}
+
+TEST_CASE("Test increment operator") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {1, 2},
+        {3, 4}};
+    g.loadGraph(graph);
+
+    ++g;
+    vector<vector<int>> expected = {
+        {2, 3},
+        {4, 5}};
+    CHECK(g.getMatrix() == expected);
+}
+
+TEST_CASE("Test decrement operator") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {2, 3},
+        {4, 5}};
+    g.loadGraph(graph);
+
+    --g;
+    vector<vector<int>> expected = {
+        {1, 2},
+        {3, 4}};
+    CHECK(g.getMatrix() == expected);
+}
+
+TEST_CASE("Test equality operator") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {1, 2},
+        {3, 4}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {1, 2},
+        {3, 4}};
+    g2.loadGraph(graph2);
+
+    CHECK(g1 == g2);
+}
+
+TEST_CASE("Test inequality operator") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {1, 2},
+        {3, 4}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {1, 2},
+        {3, 5}};
+    g2.loadGraph(graph2);
+
+    CHECK(g1 != g2);
+}
+
+TEST_CASE("Test less than operator") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {1, 2},
+        {3, 4}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {2, 3},
+        {4, 5}};
+    g2.loadGraph(graph2);
+
+    CHECK(g1 < g2);
+}
+
+TEST_CASE("Test greater than operator") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {2, 3},
+        {4, 5}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {1, 2},
+        {3, 4}};
+    g2.loadGraph(graph2);
+
+    CHECK(g1 > g2);
+}
+
+TEST_CASE("Test less than or equal to operator") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {1, 2},
+        {3, 4}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {1, 2},
+        {3, 4}};
+    g2.loadGraph(graph2);
+
+    CHECK(g1 <= g2);
+}
+
+TEST_CASE("Test greater than or equal to operator") {
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {2, 3},
+        {4, 5}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {1, 2},
+        {3, 4}};
+    g2.loadGraph(graph2);
+
+    CHECK(g1 >= g2);
+}
+
+TEST_CASE("Test invalid graph with non-square matrix") {
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1},
+        {1, 0},
+        {0, 1}};
     CHECK_THROWS(g.loadGraph(graph));
 }
